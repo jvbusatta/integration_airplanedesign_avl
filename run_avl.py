3815f139d51f63abe29a72eecdd8da3de20a65f2
Joao Victor Busatta <jjvictor_@hotmail.com>
2022-06-16 10:46:49 -0300

    16_06-10:46

--- run_avl.py
+++ run_avl.py
@@ -3,7 +3,6 @@ import subprocess
 import pandas as pd
 import numpy as np
 
-def open_avl():
-    subprocess.call("D:\TCC\integration_airplanedesign_avl\avl.exe < D:\TCC\integration_airplanedesign_avl\command_file.in", shell=True)
-
-open_avl()
+def avl():
+    subprocess.call("avl.exe < command_file.in", shell=True)
+avl()

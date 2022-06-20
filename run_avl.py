92a34911afe841904264476676c9760ce5f910be
Joao Victor Busatta <jjvictor_@hotmail.com>
2022-06-20 19:33:49 -0300

    commit_20-06

--- run_avl.py
+++ run_avl.py
@@ -10,8 +10,27 @@ import math
 
 def avl():
     subprocess.call("avl.exe < command_file.in", shell=True)
+
 avl()
 
+def teste():
+    
+    if os.path.exists("Aerodynamic_coefficients.txt") and os.path.exists("Aerodynamic_coefficients_5.txt"):
+
+        read_file = pd.read_csv("Aerodynamic_coefficients.txt", skiprows=8, delim_whitespace=True)
+        read_file.to_csv("Aerodynamic_coefficients.csv", index=None)
+
+        df = pd.read_csv("Aerodynamic_coefficients.csv")
+
+        CL_tot = float(df._get_value(9, 2, takeable = True))
+        CD_tot = float(df._get_value(10,2, takeable= True))
+        print('Coeficiente de sustentação: ', CL_tot)
+        print('Coeficiente de arrasto: ', CD_tot)
+
+teste()
+
+
+'''
 def aerodynamic():
 
     #taper ratio
@@ -22,18 +41,17 @@ def aerodynamic():
 
     
     #p.62 RAYMER
-    S = w/ ( w / S)
+    S = w / ( w / S)
     b = sqrt(AR * S)
-    c_root = 2 * ( S / ( b * (1 + lambda)))
+    c_root = (2 * ( S / ( b * (1 + lambda))))
     c_tip = lamba * c_root
 
     #mean chordwise
-    c_med = (2/3)*c_root*((1+ lambda + pow(lambda,2))/(1 + lambda))
+    c_med = ((2/3)*c_root*((1+ lambda + pow(lambda,2))/(1 + lambda)))
     
     #distance in the y axie 
     Y_med = (b/6)*((1 + 2 * lambda)*(1 + lambda))
 
     #aerodynamic center
     c_a = 0.25 * c_med
-
-    
+'''
